b753e85b239015e890735daa73a38e6b5cbdcde7
Eric Robinson <eric.z.robinson@gmail.com>
2013-10-29 21:28:51 -0400

    started anew

--- js/main.js
+++ js/main.js
@@ -1,17 +1,22 @@
 $( document ).ready(function() {
     // Handler for .ready() called.
+    //var myArray = [ "hello", "world", "!" ];
     
     $.getJSON("https://api.github.com/users/robinsone/repos", function(data){  
         var listItems = '';
-      
-        
-        $.each(data.items, function(i,item){
-           
-            listItems += '<p>'+i.name+'</p>';
+
+        listItems += '<ul class="list-group">';
+        $.each(data, function(i,item){
+            
+            if(item.name != "robinsone.github.com"){
+                listItems += '<li class="list-group-item"><a href="'+item.html_url+'">';
+                listItems += '<span class="badge pull-right">'+item.language+'</span>'+item.name+'</a></li>';
+            }
             
         });
-        $('div.projects').append(listItems);
-       
+        listItems += '</ul>';
+        $('#githubprojects').append(listItems);
+        
     });  
     
 });
